5897c337bcaff32e183bccd509a946ae5401f18f
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-01-28 17:16:25 +0000

    Avoid using subprocess, can't guarentee that registration has happened

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,30 +1,57 @@
 import os
 import shutil
-import subprocess
 import tempfile
 
-env = os.environ.copy()
-env['PYTHONPATH'] = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..'))
+from setuptools import Distribution
+
+from setuptools_gettext import (
+    build_mo,
+    install_mo,
+    load_pyproject_config,
+    update_pot,
+)
 
 
 def test_example_build():
     td = tempfile.mkdtemp()
     shutil.copytree("example", td + "/example")
-    subprocess.check_call(
-        ['python', 'setup.py', 'build_mo'],
-        cwd=os.path.join(td, 'example'),
-        env=env)
+
+    old_cwd = os.getcwd()
+
+    dist = Distribution()
+
+    load_pyproject_config(dist, {})
+
+    os.chdir(td + "/example")
+    try:
+        cmd = build_mo(dist)
+        cmd.initialize_options()
+        cmd.finalize_options()
+        cmd.run()
+    finally:
+        os.chdir(old_cwd)
 
 
 def test_example_install():
     td = tempfile.mkdtemp()
     shutil.copytree("example", td + "/example")
     root = tempfile.mkdtemp()
-    subprocess.check_call(
-        ['python', 'setup.py', 'install_mo', f'--root={root}'],
-        cwd=os.path.join(td, 'example'),
-        env=env)
+
+    dist = Distribution()
+
+    load_pyproject_config(dist, {})
+
+    old_cwd = os.getcwd()
+
+    os.chdir(td + "/example")
+    try:
+        cmd = install_mo(dist)
+        cmd.initialize_options()
+        cmd.root = root  # type: ignore
+        cmd.finalize_options()
+        cmd.run()
+    finally:
+        os.chdir(old_cwd)
 
 
 def test_update_pot():
@@ -32,10 +59,24 @@ def test_update_pot():
     shutil.copytree("example", td + "/example")
     p = os.path.join(td, 'example', 'hallowereld', 'example.py')
     with open(p, 'w') as f:
+        f.write("# Use the \"_\" shorthand for gettext\n")
+        f.write("from gettext import gettext as _\n")
         f.write('print(_("Hello Example"))')
-    subprocess.check_call(
-        ['python', 'setup.py', 'update_pot'],
-        cwd=os.path.join(td, 'example'),
-        env=env)
+
+    dist = Distribution(attrs={
+        "name": "hallowereld",
+    })
+
+    load_pyproject_config(dist, {})
+
+    old_cwd = os.getcwd()
+    os.chdir(os.path.join(td, 'example'))
+    try:
+        cmd = update_pot(dist)
+        cmd.initialize_options()
+        cmd.finalize_options()
+        cmd.run()
+    finally:
+        os.chdir(old_cwd)
     with open(os.path.join(td, 'example', 'po', 'hallowereld.pot')) as f:
         assert 'Hello Example' in f.read()
